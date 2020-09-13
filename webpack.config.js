814629c40c1148b05c2a769a08938b482b8c2a49
NessMC <contact@nessmc.fr>
2020-09-13 11:50:23 +0200

    🔧 Webpack configuration file added.

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+module.exports = {
+  entry: './src/index.ts',
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  node: {
+    fs: 'empty'
+  },
+  resolve: {
+    extensions: [ '.ts' ],
+    alias: {
+      interfaces: path.resolve(__dirname, 'src/interfaces'),
+      core: path.resolve(__dirname, 'src/core'),
+      utils: path.resolve(__dirname, 'src/utils'),
+      tests: path.resolve(__dirname, 'tests'),
+    }
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
