15c1e94ef725c7dbd63c763f5bb0f3fbdeebe1be
NessMC <contact@nessmc.fr>
2020-09-15 20:51:47 +0200

    🔥 Unused type removed from Node interface.

--- src/interfaces/node.ts
+++ src/interfaces/node.ts
@@ -1,8 +1,6 @@
-import { Expression } from 'jsep';
-
 export interface Node {
   type?: string,
-  value?: null | string | Expression,
+  value?: null | string,
   id?: number,
   depth?: number,
   children: Array<Node | undefined>,
