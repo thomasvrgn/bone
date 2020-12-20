298e1da95f2c3b6ab76e60e3abab00d082288fee
NessMC <contact@nessmc.fr>
2020-12-20 11:28:13 +0100

    ✨ Lexer class added.

--- src/core/lexer.ts
+++ src/core/lexer.ts
@@ -9,3 +9,21 @@ export interface Token {
   token: Tokens,
   value: string,
 }
+
+export class Lexer {
+  private static formatSource(source: string): string {
+    return source
+      .split(/\r?\n/g)
+      .map(line => line.trim())
+      .filter(line => line.length > 0)
+      .join('');
+  }
+
+  public static tokenize(source: string): Token[] {
+    const code: string = this.formatSource(source);
+    for (const char of code) {
+      console.log(char);
+    }
+    return [];
+  }
+}
