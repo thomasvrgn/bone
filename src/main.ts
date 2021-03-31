f7b89b25e7d73ed1e8c03c88902fcc4cd43be387
thomasvergne <thomas@quark-lang.dev>
2021-03-31 20:48:35 +0200

    ➕ Import updated.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { readFile } from 'fs-extra';
 import path from 'path';
-import { Lexer } from 'core/lexer';
+import { Lexer } from '@core/lexer';
 
 export async function main() {
   const sample: string = await readFile(path.join(__dirname, '..', 'sample', 'index.bone'), 'utf-8');
