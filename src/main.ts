0eec8ed44d2ad365941b678af059a259f2df68e7
thomasvergne <thomas@quark-lang.dev>
2021-04-01 12:56:24 +0200

    ✨ Lexer tokens definition added.

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,19 @@ import { Lexer } from '@core/lexer';
 
 export async function main() {
   const sample: string = await readFile(path.join(__dirname, '..', 'sample', 'index.bone'), 'utf-8');
-  console.log(Lexer.tokenize(sample));
-  console.log('Hello from Bone.');
+  const tokens = {
+    String: /".*?"/,
+    Comma: /,/,
+    Node: /[{}]/,
+    Bracket: /[[\]]/,
+    Definition: /!def/,
+    Operator: /[=+]/,
+    Integer: /\d+/,
+    Word: /\w+/,
+  };
+  for (const token of Lexer.tokenize(sample, tokens)) {
+    console.log(token);
+  }
 }
 
 main();
